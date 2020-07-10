74d7b094d6a38f586a6db323441ac2607a2c27ef
Paul Onteri <onteripaul@gmail.com>
2020-07-10 18:45:28 +0300

    handled code exec ution errors

--- backend/services/python.js
+++ backend/services/python.js
@@ -1,32 +1,38 @@
-var fs = require("fs");
-var exec = require("child_process").exec;
+const fs = require("fs");
+const exec = require("child_process").exec;
 const { v1: uuidv1 } = require("uuid");
-
 const path = process.env.CODE_PATH || "../../";
+const timeOut = process.env.TIME_OUT || 5000;
 
 run = (code, func) => {
   var filename = uuidv1();
 
-  // create python file
   fs.writeFile(path + filename + ".py", code, function (err) {
     if (err) {
       console.log("Error creating file: " + err);
     } else {
-      //
-      // run python
       var command = "python " + path + filename + ".py";
-      exec(command, function (error, stdout, stderr) {
-        // handle execution errors
+      exec(command, { timeout: timeOut }, function (error, stdout, stderr) {
         if (error) {
-          console.log("Error:" + err);
-          console.log("stderr:" + stderr);
-          func({ error: stderr });
-        }
-        // success
-        else {
+          console.log(error);
+          console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+          console.log(stderr);
+
+          if (error.toString().includes("ERR_CHILD_PROCESS_STDIO_MAXBUFFER")) {
+            errorMessage =
+              "Process terminated. 'maxBuffer' exceeded. This normally happened during an infinite loop.";
+          } else if (error.signal === "SIGTERM") {
+            errorMessage =
+              "Process terminated. Please check your code and try again.";
+          } else if (stderr) {
+            errorMessage = stderr;
+          } else {
+            errorMessage = "Something went wrong. Please try again";
+          }
+          func(errorMessage);
+        } else {
           console.log("Python file successfully executed !");
-          var out = { output: stdout };
-          func(out);
+          func(stdout);
         }
       });
     }
