99c3f5f7669dcf21b82c53e71f515dd3b3d2e51a
Paul Onteri <onteripaul@gmail.com>
2020-07-11 07:43:17 +0300

    simple form

--- frontend/src/components/code/Execute.js
+++ frontend/src/components/code/Execute.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import "./Execute.css";
 
 export default class Execute extends Component {
+  handleSubmit(event) {
+    var fm = document.getElementById("code-form");
+    var data = new FormData(fm);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
-        <h1>Run Code!</h1>
+        <h2>Textareas</h2>
+        <form onSubmit={this.handleSubmit} id="code-form">
+          <textarea
+            name="text"
+            className="textarea"
+            // rows="20"
+          ></textarea>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
