9d5b4c8d63f11c7f1315e1bddb1223c45834aa2a
Paul Onteri <onteripaul@gmail.com>
2020-07-12 18:34:00 +0300

    change server host

--- frontend/src/actions/Execute.js
+++ frontend/src/actions/Execute.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 import { RUN_CODE_SUCCESS, RUN_CODE_FAILED, RUN_CODE_RUNNING } from "./types";
+var server = "https://api.runcode.proj.paulonteri.com";
+
+const addr = () => {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    return "http://localhost:6500" + "/code";
+  } else {
+    return server + "/code";
+  }
+};
 
 const headers = {
   "Content-Type": "multipart/form-data",
@@ -8,7 +17,7 @@ const headers = {
 export const runCode = (code) => (dispatch, getState) => {
   dispatch({ type: RUN_CODE_RUNNING });
   axios
-    .post(`http://localhost:6500/python`, code, {
+    .post(addr(), code, {
       headers: headers,
     })
     .then((res) => {
