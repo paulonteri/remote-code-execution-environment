1cfbfe67a0efcbb3c24e6fb936d8a31c69cb5cbb
Paul Onteri <onteripaul@gmail.com>
2020-07-12 17:09:55 +0300

    Handle different langauges

--- core/Server.js
+++ core/Server.js
@@ -3,7 +3,10 @@ var formidable = require("express-formidable");
 var cors = require("cors");
 var app = express();
 var port = process.env.PORT || 6500;
-var python = require("./services/python");
+const python = require("./services/python");
+const java = require("./services/java");
+const javascript = require("./services/javascript");
+
 app.use(formidable());
 var corsOptions = {
   // origin: "http://example.com",
@@ -12,14 +15,35 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 // ROUTES
-app.get("/", (req, res) => res.send("Hi There!"));
+app.get("/", (req, res) =>
+  res.send(
+    "Hi There! You shouldn't be seeing this. 🙂 Try:{https://runcode.paulonteri.com/#/} or {https://github.com/paulonteri/remote-code-execution-environment}"
+  )
+);
 
 app.post("/python", (req, res) => {
   var text = req.fields.text;
   var language = req.fields.language;
-  python.run(text, function (data) {
-    res.status(200).json(data);
-  });
+
+  switch (language) {
+    case "python":
+      python.run(text, function (data) {
+        res.status(200).json(data);
+      });
+      break;
+    case "javascript":
+      javascript.run(text, function (data) {
+        res.status(200).json(data);
+      });
+      break;
+    case "java":
+      java.run(text, function (data) {
+        res.status(200).json(data);
+      });
+      break;
+    default:
+      res.status(422).send("Invalid programming language!");
+  }
 });
 
 app.listen(port, () =>
